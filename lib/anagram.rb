8061c52540a9cb5e5641bf2fe4cf4e1d8cc4f6be
Abdelrahman Elsirafy <rahman2408@gmail.com>
2021-01-22 08:51:55 -0500

    commit tree 1f6eb8d4673c3fe1c7adc89662315447d70f9c3c

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,27 @@
-# Your code goes here!
+# # Your code goes here!
+# class Anagram
+#     attr_accessor :detector :anagrams
+    
+#     def intialize
+#         @detector=detector
+#         @anagrams=anagrams
+#     end
+    
+#     def detector
+
+
+
+#     def anagrams
+#         @anagrams=[]
+#     end
+class Anagram < Struct.new(:initial_word)
+    def match words
+      words.select do |word|
+        self.class.anagrams?(initial_word.downcase, word.downcase)
+      end
+    end
+  
+    def self.anagrams? a, b
+      a != b and a.chars.sort == b.chars.sort
+    end
+  end
